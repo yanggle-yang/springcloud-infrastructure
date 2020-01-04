64657e90b503f5daba6d4e2467d3ad4643a5d8f5
Yanggle Yang <yanggle.yang@ecitele.com>
2020-01-04 15:16:46 +0800

    springcloud-eureka-server-ha3-security

---
+++ springcloud-eureka-server/springcloud-eureka-server-ha3-security/src/main/java/com/yanggle/eureka/server/SpringCloudEurekaServerSecurityApplication.java
@@ -0,0 +1,16 @@
+package com.yanggle.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer //开启Eureka Server
+@SpringBootApplication
+public class SpringCloudEurekaServerSecurityApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringCloudEurekaServerSecurityApplication.class, args);
+    }
+
+}
